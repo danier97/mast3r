2d71618c536e1a9a43c253d59bb4921edf420d60
Duolikun Danier <duolikun.danier@ed.ac.uk>
2025-04-14 19:37:45 +0100

    make import paths absolute and make dust3r&croco point to forked repos

--- mast3r/catmlp_dpt_head.py
+++ mast3r/catmlp_dpt_head.py
@@ -8,12 +8,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import mast3r.utils.path_to_dust3r  # noqa
-from dust3r.heads.postprocess import reg_dense_depth, reg_dense_conf  # noqa
-from dust3r.heads.dpt_head import PixelwiseTaskWithDPT  # noqa
-import dust3r.utils.path_to_croco  # noqa
-from models.blocks import Mlp  # noqa
-from models.dpt_block import Interpolate  # noqa
+import submodules.mast3r.mast3r.utils.path_to_dust3r  # noqa
+from submodules.mast3r.dust3r.dust3r.heads.postprocess import reg_dense_depth, reg_dense_conf  # noqa
+from submodules.mast3r.dust3r.dust3r.heads.dpt_head import PixelwiseTaskWithDPT  # noqa
+import submodules.mast3r.dust3r.dust3r.utils.path_to_croco  # noqa
+from submodules.mast3r.dust3r.croco.models.blocks import Mlp  # noqa
+from submodules.mast3r.dust3r.croco.models.dpt_block import Interpolate  # noqa
 
 
 def reg_desc(desc, mode):
--- mast3r/model.py
+++ mast3r/model.py
@@ -8,11 +8,11 @@ import torch
 import torch.nn.functional as F
 import os
 
-from mast3r.catmlp_dpt_head import mast3r_head_factory
+from submodules.mast3r.mast3r.catmlp_dpt_head import mast3r_head_factory
 
-import mast3r.utils.path_to_dust3r  # noqa
-from dust3r.model import AsymmetricCroCo3DStereo  # noqa
-from dust3r.utils.misc import transpose_to_landscape  # noqa
+import submodules.mast3r.mast3r.utils.path_to_dust3r  # noqa
+from submodules.mast3r.dust3r.dust3r.model import AsymmetricCroCo3DStereo  # noqa
+from submodules.mast3r.dust3r.dust3r.utils.misc import transpose_to_landscape  # noqa
 
 
 inf = float('inf')
